f67a37264679c66cd856bf003c919f386d363c8d
Alexander <77795206+Abragadabra@users.noreply.github.com>
2021-11-09 22:17:03 +0500

    edit: main.py
    Removed unnecessary comments and made the code more beautiful :)

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ def factorial(iter):
 	if iter == 0:
 		return 1
 	elif iter < 0:
-		#Вместо возврата строки лучше вызвать ValueError
-		#Так не рекомендую делать - return 'Error! The number must be non-negative!'
-		#Лучше делать так:
 		raise ValueError('The number must be non-negative!')
 	else:
 		return iter * factorial(iter - 1)
 
-#В остальном всё ок
+
 print(factorial(int(input('>>'))))
